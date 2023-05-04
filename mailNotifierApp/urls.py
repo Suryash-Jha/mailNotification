71c2e8269cb8a6e6fb239e37ba4d9a49f9d20b80
Suryash <joblesscoder2468@gmail.com>
2023-05-04 23:45:32 +0530

    totCountIncreasing with each message.created

--- mailNotifierApp/urls.py
+++ mailNotifierApp/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('api/', views.callApi, name="api"),
+    path('clientApi/', views.clientApi, name="clientApi"),
+
 
 ]
--- mailNotifierApp/views.py
+++ mailNotifierApp/views.py
@@ -4,6 +4,8 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 import json
 
+totMailCount = 0
+
 
 @csrf_exempt
 def index(request):
@@ -17,6 +19,12 @@ def callApi(request):
     if (request.method == 'POST'):
         json_data = request.body.decode('utf-8')
         data = json.loads(json_data)
+        delta = data['deltas'][0]
+        typeOfDelta = delta['type']
+        if typeOfDelta == 'message.created':
+            global totMailCount
+            totMailCount += 1
+
         response_data = {
             'data': data,
             'status': 'success',
@@ -24,3 +32,14 @@ def callApi(request):
         }
 
     return JsonResponse(response_data)
+
+
+@csrf_exempt
+def clientApi(request):
+    if request.method == 'GET':
+        return JsonResponse({'status': 'success', 'count': totMailCount})
+
+
+# {'deltas': [
+#     {'date': 1683222145, 'object': 'message', 'type': 'message.created', 'object_data': {'namespace_id': '53egjzdr5nnhnrnztgpwswjbr', 'account_id': '53egjzdr5nnhnrnztgpwswjbr', 'object': 'message', 'attributes': {'thread_id': 'bui5b7061388dljl6c8ukvyd8', 'received_date': 1683222125}, 'id': 'edm9zwql22p14mpd22iczts2i', 'metadata': None}}]
+#  }
