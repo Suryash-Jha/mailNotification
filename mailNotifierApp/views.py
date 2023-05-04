1647b90ecaf18d7ea9f42f4fdf640368cedaa0c5
Suryash <joblesscoder2468@gmail.com>
2023-05-04 23:13:42 +0530

    Webhook working fine and response getting in the function

--- mailNotifierApp/views.py
+++ mailNotifierApp/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-
+from django.http import FileResponse, JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+import json
 
 
+@csrf_exempt
 def index(request):
     return render(request, "mailNotifierApp/index.html")
+
+
+@csrf_exempt
+def callApi(request):
+    data = {}
+    response_data = {}
+    if (request.method == 'POST'):
+        json_data = request.body.decode('utf-8')
+        data = json.loads(json_data)
+        response_data = {
+            'data': data,
+            'status': 'success',
+
+        }
+
+    return JsonResponse(response_data)
